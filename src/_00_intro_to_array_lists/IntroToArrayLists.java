967901f4594e0c65430057856fe76ecf79e13904
league <league@imac-14.attlocal.net>
2022-08-22 18:31:31 -0700

    k

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,76 @@
 package _00_intro_to_array_lists;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
+		String listh;
+		int i;
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList (java.util) class
-
+		ArrayList<String> hats = new ArrayList<String>();
+		ArrayList<Integer> not = new ArrayList<Integer>();
+		ArrayList<String> space = new ArrayList<String>();
 		//2. Add five Strings to your list
+		hats.add("Baseball cap");
+		hats.add("beanie");
+		hats.add("fedora");
+		hats.add("top hat");
+		hats.add("bereau");
+		for(i=0; hats.size()>i;i++) {
+		space.add(String.format("%-20s", hats.get(i)));
+		}
+		for(i=0;i<hats.size();i++) {
+			hats.set(i, space.get(i)+(i+1));
 		
+		}
 		//3. Print all the Strings using a standard for-loop
-		
+		for( i=0; i < hats.size(); i++) {
+	
+			System.out.println( hats.get(i));
+		}
+		System.out.println("\n");
 		//4. Print all the Strings using a for-each loop
-		
+		for(String cycle : hats) {
+			System.out.println(cycle);
+		}
+		System.out.println("\n");
 		//5. Print only the even numbered elements in the list.
+		for( i=0; i < hats.size(); i++) {
+			if(i%2==1) {
 		
+			System.out.println(	hats.get(i));
+			}
+		}
+
+		System.out.println("\n");
 		//6. Print all the Strings in reverse order.
-		
+		int tally = hats.size();
+		for(i=0; i < hats.size(); i++) {
+			
+			listh = hats.get(tally-1);
+			System.out.println(listh);
+
+			tally--;
+		}
+		System.out.println("\n");
 		//7. Print only the Strings that have the letter 'e' in them.
+
+		for( i=0; i < hats.size(); i++) {
+			if(hats.get(i).contains("e")) {
+				
+				System.out.println(hats.get(i));
+			}
+			else{
+				not.add(i);
+			}
+		}
+System.out.print("\n");
+		if(not.size()>0) {
+		for(i=0; i<not.size(); i++) {
+			System.out.println(hats.get(not.get(i)));
+		}
+		}
 	}
 }
--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -4,27 +4,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 import _01_array_list_visualizer.data.ArrayList;
 
 public class VisualArrayLists implements ActionListener {
 	Timer timer;
-	JFrame frame = new JFrame();
-	
+	JFrame jeff = new JFrame();
+	int i;
+	int tally=0;
+	String search = "person";
 	ArrayList<String> list = new ArrayList<String>();
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("burger");
+		list.add("taco");
+		list.add("rat");
+		list.add("app");
+		list.add("bar");
 		// 2. Set one item in our list to be something different
-		
+		list.set(3, "bat");
 		// 3. Remove something from our list
-		
+		list.remove(2);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
+		for(i=0; i<list.size();i++) {
+			if(list.get(i).contains(search)) {
+			tally = 1;
+			}
+		}
+		if(tally==0) {
+			JOptionPane.showMessageDialog(null,"A " + search + " is not in the list.");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "A " + search + " is n̶o̶t̶ in the list.");
+		}
 		
 		// 6. Get a specific item from our list and print it out
 		
@@ -35,7 +53,7 @@ public class VisualArrayLists implements ActionListener {
 	
 	
 	/*
-	 * ------------DONT CHANGE ANYTHING BELOW THIS LINE-----------------------------------------
+	 * ------------DON̶T̶ CHANGE ANYTHING BELOW THIS LINE-----------------------------------------
 	 */
 	public static void main(String[] args) {
 		VisualArrayLists vcc = new VisualArrayLists();
@@ -43,13 +61,13 @@ public class VisualArrayLists implements ActionListener {
 	}
 
 	void setup() {
-		timer = new Timer(1000 / 60, this);
+		timer = new Timer(1000 / 160, this);
 		timer.start();
-		frame.setVisible(true);
-		frame.setTitle("LEAGUE ArrayList Visualizer");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(list.displayPanel);
-		frame.pack();
+		jeff.setVisible(true);
+		jeff.setTitle("LEAGUE ArrayList Visualizer");
+		jeff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jeff.add(list.displayPanel);
+		jeff.pack();
 		addThingsToList();
 	}
 
