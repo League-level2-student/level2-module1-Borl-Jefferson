45ee70802a63cadab196026e49d221f22b64b5bb
league <league@imac-14.attlocal.net>
2022-08-29 18:29:21 -0700

    xdh

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,6 +1,17 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import _01_array_list_visualizer.data.ArrayList;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +20,38 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	ArrayList<String> names = new ArrayList<String>();
+
+	JFrame jeff = new JFrame();
+	JPanel jepp = new JPanel();
+	JButton jeb1 = new JButton();
+	JButton jeb2 = new JButton();
+	int tally=1;
+void run() {
+	jeb1.addActionListener(this);
+	jeb2.addActionListener(this);
+	jeb1.setText("Choose a name");
+	jeb2.setText("List the names");
+	jepp.add(jeb1);
+	jepp.add(jeb2);
+	jeff.add(jepp);
+	jeff.pack();
+	jeff.setVisible(true);
+	jeff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getSource()==jeb1) {
+		String input = JOptionPane.showInputDialog("Pick a name.");
+		names.add("Guest #" + tally + " " + input);
+		tally++;
+	}
+	else {
+		for(int i=0; i<names.size();i++) {
+			System.out.println(names.get(i));
+		}
+	}
+}
 }
---
+++ src/_02_array_list_guestbook/Runner.java
@@ -0,0 +1,10 @@
+package _02_array_list_guestbook;
+
+public class Runner {
+
+public static void main(String[] args) {
+	GuestBook a = new GuestBook();
+a.run();	
+}
+
+}
